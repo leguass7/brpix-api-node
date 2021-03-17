167b0146dbd2f6d9b48e51f757a48242215ca58d
leguass7 <leandro.sbrissa@hotmail.com>
2021-03-16 21:09:35 -0300

    ci(build): commit automatized, delete builds

--- dist/ApiPix/api/onResponseError.d.ts
+++
@@ -1,5 +0,0 @@
-import { IApiResponseError } from '../types/responses-types';
-export declare function onResponseError(error: any): PromiseLike<{
-    data?: IApiResponseError;
-}>;
-//# sourceMappingURL=onResponseError.d.ts.map
